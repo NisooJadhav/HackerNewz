81e285893a05a6e423899325b249b522e3265344
nisoojadhav <nisoojadhav@gmail.com>
2023-10-01 17:49:35 +0530

    improvements

--- server.mjs
+++ server.mjs
@@ -10,7 +10,19 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.get("/", (req, res) => {
-  const url =
+  const rssUrl =
     "https://news.google.com/rss/search?q=hacker&hl=en-US&gl=US&ceid=US:en";
-  fetch(url).then(res);
+  fetch(rssUrl)
+    .then((response) => response.text())
+    .then((xml) => {
+      res.setHeader("Content-Type", "text/xml");
+      res.send(xml);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      res.status(500).send("An error occurred while fetching the RSS feed.");
+    });
+});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
